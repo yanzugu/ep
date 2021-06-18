6c9244122b978c95a02c8af8b996bbab6c4062af
yanzugu <yanzugu@gmail.com>
2021-06-18 09:59:14 +0800

    Update footer

--- js/footer.js
+++ js/footer.js
@@ -1,17 +1,66 @@
 const footer = {
     template: `
     <footer>
-    <p class="cpr">國立嘉義大學　版權所有　Copyright © 2017 All Rights Reserved.</p>
-    <p class="detail">
-        國立嘉義大學 電子物理學系<br>
-        600355 嘉義市 學府路 300 號<br>
-        系辦公室 電話：05-271-7911，傳真：05-271-7909<br><br>
-        Department of Electrophysics, National Chiayi University<br>
-        300 Syuefu Road, Chiayi City 600355, Taiwan<br>
-        Phone: +886-5-271-7911; Fax: +886-5-271-7909<br>
-        E-mail: dap@mail.ncyu.edu.tw
-    </p>
-</footer>
+            <div class="wrap">
+                <div class="footer-item">
+                    <p class="cpr">國立嘉義大學　版權所有　Copyright © 2017 All Rights Reserved.</p>
+                    <p class="detail">
+                        國立嘉義大學 電子物理學系<br>
+                        600355 嘉義市 學府路 300 號<br>
+                        系辦公室 電話：05-271-7911，傳真：05-271-7909<br><br>
+                        Department of Electrophysics, National Chiayi University<br>
+                        300 Syuefu Road, Chiayi City 600355, Taiwan<br>
+                        Phone: +886-5-271-7911; Fax: +886-5-271-7909<br>
+                        E-mail: dap@mail.ncyu.edu.tw
+                    </p>
+                </div>
+                <div class="footer-link footer-item">
+                    <div>
+                        <ul>
+                            <li style="text-align: left; width: 100%;">
+                                <h5 style="color: #fff;">導覽連結</h1>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="../bulletin/more.html">公佈欄</a></li>
+                            <li><a href="../honor/more.html">榮譽榜</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="../about/history.html">系所歷史沿革</a></li>
+                            <li><a href="../about/location.html">系所地理位置</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="../about/contact.html">系所聯絡資訊</a></li>
+                            <li><a href="../teaching-and-researching/teachers.html">專任教師列表</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="../teaching-and-researching/lab.html">特色研究實驗室</a></li>
+                            <li><a href="../student/lesson-map.html">必選修 / 課程地圖</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="../student/student-association-and-class.html">系學會與班級專欄</a></li>
+                            <li><a href="../freshman-and-alumni/recruit.html">招生資訊</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="../freshman-and-alumni/outstading-alumni.html">傑出系友</a></li>
+                            <li><a href="../freshman-and-alumni/alumni.html">系友會資訊</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     `
 };
 
